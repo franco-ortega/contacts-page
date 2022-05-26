05cf140caaa16ed10c892c80eeb4d8dbca76adf3
Franco Ortega <francopdx@gmail.com>
2022-05-26 10:24:12 -0700

    add Loading component

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from 'Loading.module.css';
+
+const Loading = () => {
+  return (
+    <p className={styles.Loading}>Contacts are loading...</p>
+  );
+};
+
+export default Loading;
